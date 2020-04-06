3074d34285431c5a7e7dc0b60820c5ded3e6d617
Maxim Fazylov <m.fazylov@gradient.ru>
2020-04-06 12:34:17 +0500

    second commit

--- src/main/kotlin/HttpControllers.kt
+++ src/main/kotlin/HttpControllers.kt
@@ -1,17 +1,17 @@
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/sign")
 class RootController(private val signer: Signer) {
+    val content: String = ""
 
+    @PostMapping("/", consumes = ["application/json"], produces = ["application/json"])
+    fun sign(@RequestBody body: String = ""): String {
+        return "{'test':'OK'}"
+    }
 
-    @GetMapping("/")
-    fun findAll() = signer.sign();
 
 }
--- src/main/kotlin/Signer.kt
+++ src/main/kotlin/Signer.kt
@@ -1,31 +1,49 @@
-import com.sun.tools.javac.util.Convert
-import org.bouncycastle.crypto.CipherParameters
+import org.bouncycastle.cert.jcajce.JcaCertStore
+import org.bouncycastle.cms.CMSProcessableByteArray
+import org.bouncycastle.cms.CMSSignedDataGenerator
+import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder
 import org.bouncycastle.crypto.Signer
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder
+import org.bouncycastle.util.Store
 import org.bouncycastle.util.encoders.Base64
 import java.io.ByteArrayInputStream
-import java.security.Key
 import java.security.KeyStore
-import java.security.cert.Certificate
+import java.security.PrivateKey
+import java.security.cert.X509Certificate
+import java.util.*
 
 
 class Signer {
-
-    fun sign(content: String, password: String, pfxBase64: String) {
-        val signer:Signer
-        val ks : KeyStore = KeyStore.getInstance("PKCS12", "BC")
-        val pfxContent:ByteArray = Base64.decode(pfxBase64)
-        val baos:ByteArrayInputStream = ByteArrayInputStream(pfxContent)
+    fun sign(
+            content: String,
+            password: String,
+            pfxBase64: String
+    ): String {
+        val signer: Signer
+        val ks: KeyStore = KeyStore.getInstance("PKCS12", "BC")
+        val pfxContent: ByteArray = Base64.decode(pfxBase64)
+        val baos: ByteArrayInputStream = ByteArrayInputStream(pfxContent)
         ks.load(baos, password.toCharArray())
-        with (ks)
+        with(ks)
         {
             for (alias in aliases()) {
                 if (isKeyEntry(alias)) {
-                    val key: Key = getKey(alias, password.toCharArray())
-                    val cert: Certificate = getCertificate(alias)
-                    addKeyAndCertificateToStore()
+                    val key: PrivateKey = getKey(alias, password.toCharArray()) as PrivateKey
+                    val cert: X509Certificate = getCertificate(alias) as X509Certificate
+                    val certList = ArrayList<X509Certificate>()
+                    certList.add(cert)
+                    val certs = JcaCertStore(certList)
+                    var gen = CMSSignedDataGenerator()
+                    var signer = org.bouncycastle.operator.jcajce.JcaContentSignerBuilder("GOST3411WITHECGOST3410-2012-256").setProvider("BC").build(key)
+                    gen.addSignerInfoGenerator(JcaSignerInfoGeneratorBuilder(JcaDigestCalculatorProviderBuilder().setProvider("BC").build()).build(signer, cert))
+                    gen.addCertificates(certs)
+                    val msg = CMSProcessableByteArray(content.toByteArray())
+                    val sigData = gen.generate(msg, false)
+                    val sign = sigData.encoded
+                    return Base64.toBase64String(sign)
                 }
             }
         }
-
+        return ""
     }
 }
